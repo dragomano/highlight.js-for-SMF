35622419281ba834762638a8f2b41957c997194a
Bugo <bugo@dragomano.ru>
2021-01-19 12:15:46 +0500

    Update to v0.8
    * Add line numbers feature
    * Update highlight assets
    * Replace div with figure (html5 tag)

--- Sources/Class-Highlighting.php
+++ Sources/Class-Highlighting.php
@@ -9,7 +9,7 @@
  * @copyright 2010-2021 Bugo
  * @license https://opensource.org/licenses/BSD-3-Clause BSD
  *
- * @version 0.7
+ * @version 0.8
  */
 
 if (!defined('SMF'))
@@ -31,6 +31,7 @@ class Code_Highlighting
 		add_integration_function('integrate_admin_search', __CLASS__ . '::adminSearch', false, __FILE__);
 		add_integration_function('integrate_modify_modifications', __CLASS__ . '::modifyModifications', false, __FILE__);
 		add_integration_function('integrate_bbc_codes', __CLASS__ . '::bbcCodes', false, __FILE__);
+		add_integration_function('integrate_prepare_display_context', __CLASS__ . '::prepareDisplayContext', false, __FILE__);
 		add_integration_function('integrate_credits', __CLASS__ . '::credits', false, __FILE__);
 	}
 
@@ -51,10 +52,12 @@ class Code_Highlighting
 		// Paths
 		if (!empty($modSettings['ch_cdn_use'])) {
 			$context['ch_jss_path'] = 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@' . CH_VER . '/build/highlight.min.js';
+			$context['ch_dln_path'] = 'https://cdn.jsdelivr.net/npm/highlightjs-line-numbers.js@2/dist/highlightjs-line-numbers.min.js';
 			$context['ch_clb_path'] = 'https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js';
 			$context['ch_css_path'] = 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@' . CH_VER . '/build/styles/' . $modSettings['ch_style'] . '.min.css';
 		} else {
 			$context['ch_jss_path'] = $settings['default_theme_url'] . '/scripts/highlight.pack.js';
+			$context['ch_dln_path'] = $settings['default_theme_url'] . '/scripts/highlightjs-line-numbers.min.js';
 			$context['ch_clb_path'] = $settings['default_theme_url'] . '/scripts/clipboard.min.js';
 			$context['ch_css_path'] = $settings['default_theme_url'] . '/css/highlight/' . $modSettings['ch_style'] . '.css';
 		}
@@ -75,11 +78,13 @@ class Code_Highlighting
 
 		if (!in_array($context['current_action'], array('helpadmin', 'printpage')))
 			$context['insert_after_template'] .= '
-		<script src="' . $context['ch_jss_path'] . '"></script>
+		<script src="' . $context['ch_jss_path'] . '"></script>' . (!empty($modSettings['ch_line_numbers']) ? '
+		<script src="' . $context['ch_dln_path'] . '"></script>' : '') . '
 		<script src="' . $context['ch_clb_path'] . '"></script>
 		<script>
 			hljs.tabReplace = "' . $tab . '";
-			hljs.initHighlightingOnLoad();
+			hljs.initHighlightingOnLoad();' . (!empty($modSettings['ch_line_numbers']) ? '
+			hljs.initLineNumbersOnLoad();' : '') . '
 			window.addEventListener("load", function() {
 				let pre = document.getElementsByTagName("code");
 				for (let i = 0; i < pre.length; i++) {
@@ -193,11 +198,14 @@ class Code_Highlighting
 					'large'   => 'large',
 					'x-large' => 'x-large'
 				)
-			)
+			),
+			array('check', 'ch_line_numbers')
 		);
 
-		if (!empty($modSettings['ch_enable']) && function_exists('file_get_contents'))
+		if (!empty($modSettings['ch_enable']) && function_exists('file_get_contents')) {
 			$config_vars[] = array('callback', 'ch_example');
+			$config_vars[] = '<br>';
+		}
 
 		if ($return_config)
 			return $config_vars;
@@ -236,7 +244,7 @@ class Code_Highlighting
 		$codes[] = 	array(
 			'tag' => 'code',
 			'type' => 'unparsed_content',
-			'content' => '<div class="block_code"' . (!empty($modSettings['ch_fontsize']) ? ' style="font-size: ' . $modSettings['ch_fontsize'] . '"' : '') . '><pre><code>$1</code></pre></div>',
+			'content' => '<figure class="block_code"' . (!empty($modSettings['ch_fontsize']) ? ' style="font-size: ' . $modSettings['ch_fontsize'] . '"' : '') . '><pre><code>$1</code></pre></figure>',
 			'validate' => function(&$tag, &$data, $disabled) {
 				if (!isset($disabled['code'])) {
 					$data = rtrim($data, "\n\r");
@@ -250,12 +258,30 @@ class Code_Highlighting
 			'type' => 'unparsed_equals_content',
 			'validate' => function(&$tag, &$data, $disabled) use ($txt, $modSettings) {
 				if (!isset($disabled['code'])) {
-					$tag['content'] = '<div class="codeheader">' . $txt['code'] . ': ' . $data[1] . '</div><div class="block_code"' . (!empty($modSettings['ch_fontsize']) ? ' style="font-size: ' . $modSettings['ch_fontsize'] . '"' : '') . '><pre><code class="' . $data[1] . '">' . rtrim($data[0], "\n\r") . '</code></pre></div>';
+					$tag['content'] = '<figure class="block_code"' . (!empty($modSettings['ch_fontsize']) ? ' style="font-size: ' . $modSettings['ch_fontsize'] . '"' : '') . '><figcaption class="codeheader">' . $txt['code'] . ': ' . $data[1] . '</figcaption><pre><code class="' . $data[1] . '">' . rtrim($data[0], "\n\r") . '</code></pre></figure>';
 				}
 			},
 			'block_level' => true,
 			'disabled_content' => '<pre>$1</pre>'
 		);
+	}
+
+	/**
+	 * Творим колдовство в сообщениях форума
+	 * Заменяем <br> на нормальный перенос строки, ради отображения нумерации строк
+	 *
+	 * @param array $output
+	 * @return void
+	 */
+	public static function prepareDisplayContext(&$output)
+	{
+		global $modSettings;
+
+		if (empty($modSettings['ch_line_numbers']))
+			return;
+
+		if (strpos($output['body'], '<pre>') !== false)
+			$output['body'] = strtr($output['body'], array('<br>' => "\n"));
 	}
 
 	/**
@@ -278,11 +304,15 @@ class Code_Highlighting
  */
 function template_callback_ch_example()
 {
-	global $settings, $txt;
+	global $settings, $modSettings, $txt;
 
 	if (file_exists($settings['default_theme_dir'] . '/css/admin.css'))	{
 		$file = file_get_contents($settings['default_theme_dir'] . '/css/admin.css');
 		$file = parse_bbc('[code]' . $file . '[/code]');
+
+		if (!empty($modSettings['ch_line_numbers']))
+			$file = strtr($file, array('<br>' => "\n"));
+
 		echo '</dl><strong>' . $txt['ch_example'] . '</strong>' . $file . '<dl><dt></dt><dd></dd>';
 	}
 }
